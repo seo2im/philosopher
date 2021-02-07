1204af34e8edce571e0473757ec018bb118b4a04
seo2im <seo2im6492@gmail.com>
2021-02-07 16:55:56 +0900

    re

--- philo_one/SRCS/main.c
+++ philo_one/SRCS/main.c
@@ -6,7 +6,7 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 14:07:31 by seolim            #+#    #+#             */
-/*   Updated: 2021/02/07 16:52:58 by seolim           ###   ########.fr       */
+/*   Updated: 2021/02/07 16:55:25 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,4 @@ int		main(int argc, char *argv[])
 	if (!process(&manager))
 		ft_error("PROCESS ERROR : something wrong in process", 1);
 	clear_manager(&manager);
-
-	system("leaks philo_one > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
 }
