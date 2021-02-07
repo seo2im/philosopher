c6ee1d715e8966ee610f7a64b95e473990ad4ddf
seo2im <seo2im6492@gmail.com>
2021-02-07 16:53:33 +0900

    leak check

--- philo_one/SRCS/clear.c
+++ philo_one/SRCS/clear.c
@@ -6,7 +6,7 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 22:23:43 by seolim            #+#    #+#             */
-/*   Updated: 2021/02/06 22:53:31 by seolim           ###   ########.fr       */
+/*   Updated: 2021/02/07 16:50:04 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,27 @@ void	ft_ph_free(t_ph **phs)
 	while (phs[++i])
 	{
 		if (phs[i]->info)
+		{
+			free(phs[i]->info->start_time);
 			free(phs[i]->info);
+		}
+		if (phs[i]->last_meal_time)
+			free(phs[i]->last_meal_time);
 		if (phs[i]->thread)
 			free(phs[i]->thread);
-		free(phs[i]);
+		if (phs[i]->last_meal_mutex)
+		{
+			pthread_mutex_destroy(phs[i]->last_meal_mutex);
+			free(phs[i]->last_meal_mutex);
+		}
+		
 		if (phs[i]->left_fork)
 		{
 			pthread_mutex_destroy(phs[i]->left_fork->mutex);
 			free(phs[i]->left_fork->mutex);
 			free(phs[i]->left_fork);
-			pthread_mutex_destroy(phs[i]->last_meal_mutex);
-			free(phs[i]->last_meal_mutex);
 		}
+		free(phs[i]);
 	}
 	free(phs);
 }
@@ -39,7 +48,10 @@ void	ft_ph_free(t_ph **phs)
 void	clear_manager(t_manager *manager)
 {
 	if (manager->info)
+	{
+		free(manager->info->start_time);
 		free(manager->info);
+	}
 	if (manager->phs)
 		ft_ph_free(manager->phs);
 	pthread_mutex_destroy(manager->message);
